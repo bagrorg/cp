2a122a8c83ff541942055b3ee1acce2d3d2e28db
bagrorg <kuldanvit@gmail.com>
2022-04-09 19:04:38 +0300

    + init backup api

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "cp_utils/cp_utils.h"
 #include <iostream>
+#include <unistd.h>
+#include <cstring>
 
 void validate_input(const fs::path &src, const fs::path &dst) {
     if (!fs::exists(src)) throw std::runtime_error("No such `src` file");
@@ -7,16 +9,31 @@ void validate_input(const fs::path &src, const fs::path &dst) {
     if (dst.empty()) throw std::runtime_error("Empty `dst`");
 }
 
-bool process_dst(const fs::path &p) {
+fs::path create_backup(const fs::path &p) {
+    fs::path backup = p.parent_path() / fs::path(p.filename().string() + ".bk");
+
+    errno = 0;
+    int res = linkat(0, p.string().c_str(), 0, backup.string().c_str(), 0);             //todo: copypaste???
+    if (res != 0) {
+        throw std::runtime_error("Something wrong with file: " + std::string(strerror(errno)));
+    } else {
+        std::cout << "File successfully hard-linked!" << std::endl;
+    }
+
+    return backup;
+}
+
+fs::path process_dst(const fs::path &p) {
     if (!fs::exists(p)) {
         if (!p.parent_path().empty()) {
             fs::create_directories(p.parent_path());
         }
 
-        return false;
+        return "";
     } else {
+        auto backup = create_backup(p);
         fs::remove(p);
-        return true;
+        return backup;
     }
 }
 
@@ -30,15 +47,23 @@ int main(int argc, char *argv[]) {
     fs::path dst = argv[2];
     src = fs::absolute(src);
     dst = fs::absolute(dst);
-    bool exists;
+    fs::path backup;
 
     try {
         validate_input(src, dst);
         dst = path_processing::process_path(src, dst);
-        exists = process_dst(dst);
+        backup = process_dst(dst);
 
         my_cp::copy_main(src, dst);
     } catch(const std::exception &e) {
+        if (!backup.empty()) {
+            errno = 0;
+            int res = linkat(0, backup.string().c_str(), 0, dst.string().c_str(), 0);
+            if (res != 0) {
+                throw std::runtime_error("Something wrong with file: " + std::string(strerror(errno)));         //todo: throw in catch?
+            }
+        }
+
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
