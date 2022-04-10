a44294d7827800e7dc66cb418f64dae353d2397e
bagrorg <kuldanvit@gmail.com>
2022-04-10 15:55:07 +0300

    + forgotten wrappers

---
+++ wrappers/posix_helpers.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+namespace posix_helpers {
+    enum ERROR_TYPE {
+        LINKAT_EXDEV = 0,
+        LINKAT_OTHER,
+        SYMLINK_EXCEPTION,
+        REALPATH_EXCEPTION
+    };
+
+    class WrapperException : public std::exception {
+    public:
+        explicit WrapperException(ERROR_TYPE flag_, const std::string &strerror_input);
+
+        const char *what() const noexcept override;
+
+        ERROR_TYPE get_error() const;
+
+    private:
+        ERROR_TYPE flag_;
+        std::string strerror_;
+    };
+
+    void symlink(const std::string &__from, const std::string &__to);
+    void linkat(int __fromfd, const std::string &__from, int __tofd, const std::string &__to, int __flags);
+    std::string realpath(const std::string &__name);
+}
